11caa066d5ae3783bf4ca83f473849d64628f62d
Erik Nomitch <enomitch@gmail.com>
2015-07-08 00:59:00 -0500

    Add support for subtitle and sound in terminal-notifier.

--- lib/notifier/terminal_notifier.rb
+++ lib/notifier/terminal_notifier.rb
@@ -12,8 +12,13 @@ module Notifier
         "-group", "notifier-rubygems",
         "-title", options[:title].to_s,
         "-appIcon", options.fetch(:image, "").to_s,
-        "-message", options[:message].to_s
+        "-message", options[:message].to_s,
+        "-subtitle", options.fetch(:subtitle, "").to_s
       ]
+
+      # -sound with an empty string (e.g., "") will trigger the
+      # default sound so we need to check for it.
+      command.concat(["-sound", options.fetch(:sound, "default").to_s] ) if options[:sound]
 
       Thread.new { system(*command) }.join
     end
